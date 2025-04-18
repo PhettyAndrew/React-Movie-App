5b09ed7e425fd41b5ab10291625770689708500d
PhettyAndrew <phettyandrew@gmail.com>
2025-04-18 04:44:47 +0300

    Whole App

---
+++ frontend/src/Services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "c6b184b3c4654fe896f0cf0a86d5347a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  return data.results;
+};
